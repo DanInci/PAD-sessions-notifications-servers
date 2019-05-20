a7e45d9c3741bfd8fd3497939467b4ddc9366b45
Daniel Incicau <daniel.incicau@busymachines.com>
2019-05-21 01:20:59 +0300

    Started sessions server

---
+++ server-sessions/src/test/java/com/pad/xmen/ale/ServerSessionsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pad.xmen.ale;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServerSessionsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
